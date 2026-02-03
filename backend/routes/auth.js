d0c032f850beacc5ebfbadb9b6ed6485f51b4dad
CodewithSahilArate <sahilarate5@gmail.com>
2026-02-03 19:51:45 +0530

    the database added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,32 +2,37 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post("/register", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
-    const { name, email, phone, password } = req.body;
+    const { email, password } = req.body;
 
-    const existing = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-    if (existing) {
-      return res.status(400).json({ message: "Email already exists" });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const isMatch = await bcrypt.compare(password, user.password);
 
-    const user = new User({
-      name,
-      email,
-      phone,
-      password: hashedPassword
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      "secretkey",
+      { expiresIn: "1h" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token
     });
 
-    await user.save();
-
-    res.json({ message: "User Registered" });
-
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
